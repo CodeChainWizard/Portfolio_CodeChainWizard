820c36b3d4e25743052fd853c1586f32fce41b6d
CodeChainWizard <shubhamdanecha789@gmail.com>
2024-03-14 10:31:18 +0530

    Create protfolio Next.js and Three.js for Animation

---
+++ src/app/data.js
@@ -0,0 +1,101 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "NFT-Market Place Blockchain",
+    description: "Sell your NFTs, Digital art showcase platform",
+    date: "2023-08-15",
+    demoLink: "https://github.com/CodeChainWizard/NFT_Market.git",
+  },
+  {
+    id: 2,
+    name: "Food Delivery Flutter Full Stack App",
+    description: "Provide all functionality",
+    date: "Working",
+    demoLink: "https://github.com/CodeChainWizard/Food_App_Flutter.git",
+  },
+  {
+    id: 3,
+    name: "SaaS On BlockChain",
+    description:
+      "Build your own Decentralized pool and liquidity finder web3 tool.",
+    date: "Working",
+    demoLink: "https://github.com/CodeChainWizard/Saas_Blockchain.git",
+  },
+  {
+    id: 4,
+    name: "Drive Clone On BlockChain",
+    description: "Provide a share able drive with cloud storage on blockchain.",
+    date: "2023-08-20",
+    demoLink: "https://github.com/CodeChainWizard/DriveWEB_3.0.git",
+  },
+  {
+    id: 5,
+    name: "ChaiSutta",
+    description: "Provide a user tip to the shopkeeper in crypto.",
+    date: "2022-07-12",
+    demoLink: "https://github.com/CodeChainWizard/chaisutta.git",
+  },
+  {
+    id: 6,
+    name: "Crypto Web Using MERN Stack",
+    description: "Here, all crypto show and there details, chart and price",
+    date: "2023-6-03",
+    demoLink: "https://github.com/CodeChainWizard/p3_crypro_app.git",
+  },
+  {
+    id: 7,
+    name: "Hackthon Project",
+    description: "Provide a Map  of all Hackathon happening worldwide.",
+    date: "In development",
+    demoLink: "https://github.com/CodeChainWizard/hackathon_KumbhNavigate.git",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/CodeChainWizard",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/danecha-shubham-99660a288",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://www.x.com/Danech_Shubham",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
